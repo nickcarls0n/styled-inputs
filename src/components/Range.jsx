8d355e8b616b6ce79dea6251610d3a259d19378c
Nick <n.carlson.design@gmail.com>
2022-06-23 16:14:21 -0700

    Range component set up

--- src/components/Range.jsx
+++ src/components/Range.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import styles from "./Range.module.scss";
 
 const Range = () => {
   return (
-    <>
+    <div className={styles.range}>
+      <p>Test</p>
       <input type="range" />
-    </>
+    </div>
   );
 };
 
